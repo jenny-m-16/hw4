4a96d83e94038cf7cf8ac65d7a6cee2cbbd83700
jenny-m-16 <jenny.mak@kellogg.northwestern.edu>
2023-03-04 23:54:44 +0000

    Made some updates, not done yet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["username"]}"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Your password is incorrect. Try again."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "This email account does not exist. Please sign-up."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Thanks for stopping by! Come back soon."
+    redirect_to "/login"
   end
 end
   
